08ba00badc6e167fa211299bb3775ae25e3335e3
Chenhua <1184923569@qq.com>
2018-07-05 11:51:17 +0800

    增加数据库模型和管理页面使用模型

--- Blog/admin.py
+++ Blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from Blog.models import Blog,Category,Tag
 
 # Register your models here.
+class BlogAdmin(admin.ModelAdmin):
+	list_display = ["title","click_nums","create_time","modify_time"]
+		
+
+admin.site.register(Blog,BlogAdmin)
+admin.site.register(Category)
+admin.site.register(Tag)
